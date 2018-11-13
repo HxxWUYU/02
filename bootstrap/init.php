4bb0a7c10ada81d5ee7ba87121cfa255737af776
Derrick <derrickhuang2333@gmail.com>
2018-11-14 02:16:49 +1000

    no message

---
+++ bootstrap/init.php
@@ -0,0 +1,11 @@
+<?php 
+
+if(!isset($_SESSION)) session_start();
+
+//load environment 
+require_once __DIR__ .'/../app/config/env.php';
+
+require_once __DIR__ .'/../app/routing/route.php';
+
+
+?>
