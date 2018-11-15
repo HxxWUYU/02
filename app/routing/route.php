718806c6abd9d82c90bdbf78b5558911ca835100
Derrick <derrickhuang2333@gmail.com>
2018-11-15 18:16:47 +1000

    no message

--- app/routing/route.php
+++ app/routing/route.php
@@ -1,11 +1,16 @@
 <?php 
 
 $router = new AltoRouter;
-
+echo $_SERVER['PHP_SELF']."<br>";
 $router->map('GET','/about','','about_us');
 
 $match = $router->match();
 
-var_dump($match);
+if($match){
+	echo "About us page";
+}else{
+	header($_SERVER["SERVER_PROTOCOL"].'404 Not Found');
+	echo "This is Page not found";
+}
 
 ?>
