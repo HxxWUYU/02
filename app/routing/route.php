8997b1b926eac73f1bfb5d808a4b8c725b22a787
Derrick <derrickhuang2333@gmail.com>
2018-11-15 17:44:00 +1000

    no message

--- app/routing/route.php
+++ app/routing/route.php
@@ -6,6 +6,11 @@ $router->map('GET','/about','','about_us');
 
 $match = $router->match();
 
-var_dump($match);
+if($match){
+	echo "About us page";
+}else{
+	header($_SERVER["SERVER_PROTOCOL"].'404 Not Found');
+	echo "Page not found";
+}
 
 ?>
