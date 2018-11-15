f8450a8a787f06297eedc669ff4d7084176099ee
Derrick <derrickhuang2333@gmail.com>
2018-11-15 18:36:03 +1000

    no message

--- app/routing/route.php
+++ app/routing/route.php
@@ -3,7 +3,7 @@
 $router = new AltoRouter;
 //echo $_SERVER['PHP_SELF']."<br>";
 
-echo dirname(dirname(__FILE__));
+echo dirname(dirname("../"))."<br>";
 $router->map('GET','/02/public/about','','about_us');
 
 $match = $router->match();
