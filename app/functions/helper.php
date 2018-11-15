bcd2a3895101585131e30b5764321f118482f652
Derrick <derrickhuang2333@gmail.com>
2018-11-15 21:28:40 +1000

    no message

--- app/functions/helper.php
+++ app/functions/helper.php
@@ -6,6 +6,6 @@ use Philo\Blade\Blade;
 		$view = __DIR__ . '/../../resources/views';
 		$cache = __DIR__ . '/../../bootstrap/cache';
 		$blade = new Blade($view,$cache);
-		echo ($blade->view()->make($path)->render());
+		echo ($blade->view()->make($path,$data)->render());
 	}
 ?>
