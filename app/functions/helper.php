c2b8215c3573ffef011b37e414f0aa4772d2108f
Derrick <derrickhuang2333@gmail.com>
2018-11-15 23:41:33 +1000

    no message

--- app/functions/helper.php
+++ app/functions/helper.php
@@ -4,11 +4,13 @@ use Philo\Blade\Blade;
 
 function view($path, array $data = [])
 {
-    $view = __DIR__ . '/../../resources/views';
-    $cache = __DIR__ . '/../../bootstrap/cache';
+    // $view = __DIR__ . '/../../resources/views';
+    // $cache = __DIR__ . '/../../bootstrap/cache';
     
-    $blade = new Blade($view, $cache);
+    // $blade = new Blade($view, $cache);
     
    //echo $blade->view()->make($path, $data)->render();
     //$blade->view();
 }
+
+?>
