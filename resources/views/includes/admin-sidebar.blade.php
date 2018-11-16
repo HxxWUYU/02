85255b6edfe07db599ef4f107b21295e94af7da3
Derrick <derrickhuang2333@gmail.com>
2018-11-17 09:47:09 +1000

    no message

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -1,5 +1,5 @@
 
-  <div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
+  <div class="off-canvas position-left reveal-for-large nav" id="offCanvas" data-off-canvas>
 
     <!-- Close button -->
     <button class="close-button" aria-label="Close menu" type="button" data-close>
