e383fd2ebad60442b6f80c5b8aa8f3504c412066
Derrick <derrickhuang2333@gmail.com>
2018-11-17 09:31:44 +1000

    no message

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -4,34 +4,16 @@
 	<meta charset="utf-8">
 	<title>Admin Panel - @yield('title')</title>
 	<link rel="stylesheet" href="/02/public/css/all.css">
+	<script src="https://use.fontawesome.com/82337c0b5c.js"></script>
 </head>
 <body>
 
-  <div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
-
-    <!-- Close button -->
-    <button class="close-button" aria-label="Close menu" type="button" data-close>
-      <span aria-hidden="true">&times;</span>
-    </button>
-
-    <!-- Side bar-->
-    <ul class="vertical menu">
-      <li><a href="#">Foundation</a></li>
-      <li><a href="#">Dot</a></li>
-      <li><a href="#">ZURB</a></li>
-      <li><a href="#">Com</a></li>
-      <li><a href="#">Slash</a></li>
-      <li><a href="#">Sites</a></li>
-    </ul>
-    <!-- End Side bar-->
-
-  </div>
-
+ @include('includes.admin-sidebar')
   <div class="off-canvas-content" data-off-canvas-content>
     <!-- Your page content lives here -->
     <div class="title-bar">
   <div class="title-bar-left">
-    <button class="menu-icon hide" type="button" data-open="offCanvas"></button>
+    <button class="menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
     <span class="title-bar-title">{{getenv("APP_NAME")}}</span>
   </div>
  
---
+++ resources/views/includes/admin-sidebar.blade.php
@@ -0,0 +1,27 @@
+
+  <div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
+
+    <!-- Close button -->
+    <button class="close-button" aria-label="Close menu" type="button" data-close>
+      <span aria-hidden="true">&times;</span>
+    </button>
+
+    <h3>Welcome Admin</h3>
+    <div>
+      
+      <img src='/02/public/images/Hxx.jpg' alt="Hxx" title="Admin">
+      <p>Hxx</p>
+    </div>
+    <!-- Side bar-->
+    <ul class="vertical menu">
+      <li><a href="#"><i class="fa fa-tachometer fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
+      <li><a href="#"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Product</a></li>
+      <li><a href="#"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp; Manage Product</a></li>
+      <li><a href="#"><i class="fa fa-compress" aria-hidden="true"></i>&nbsp; Categories</a></li>
+      <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; View Orders</a></li>
+      <li><a href="#"><i class="fa fa-money fa-fw" aria-hidden="true"></i>&nbsp; Payments</a></li>
+      <li><a href="#"><i class="fa fa-sign-out fa-fw" aria-hidden="true"></i>&nbsp; Logout</a></li>
+    </ul>
+    <!-- End Side bar-->
+
+  </div>
