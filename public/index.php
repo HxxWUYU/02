76d40465162cb0423ea59134ff07a1a1c38b91a5
Derrick <derrickhuang2333@gmail.com>
2018-11-16 15:19:49 +1000

    no message

--- public/index.php
+++ public/index.php
@@ -5,9 +5,9 @@ $app_name = getenv('APP_NAME');
 use Illuminate\Database\Capsule\Manager as Capsule;
 //var_dump(in_array('mod_rewrite', apache_get_modules()));
 //echo phpinfo();
-$categories = Capsule::table('categories')->where('id',1)->first();
+//$categories = Capsule::table('categories')->where('id',1)->first();
 
-//$categories = Capsule::table('categories')->get();
+$categories = Capsule::table('categories')->get();
 
 var_dump($categories->toArray());
 
