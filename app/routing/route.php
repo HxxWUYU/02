dde9cd2580b25719696b3e2cae15731f04072eab
Derrick <derrickhuang2333@gmail.com>
2018-11-16 21:06:16 +1000

    no message

--- app/routing/route.php
+++ app/routing/route.php
@@ -4,6 +4,9 @@ $router = new AltoRouter;
 //echo $_SERVER['PHP_SELF']."<br>";
 
 $router->map('GET','/02/public/','App\Controllers\IndexController@show','home'); //@sign can be custom 
+
+//for admin routes
+$router->map('GET','/02/public/admin','App\Controllers\Admin\DashboardController@show','admin_dashboard'); //@sign can be custom 
 //$match = $router->match()  match is an array containing 3 elements:
 //traget, parmas, name // target is the App\..\ name is home
 
