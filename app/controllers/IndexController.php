17dffe5a8c975a71631a1abdb5545345b18572c5
Derrick <derrickhuang2333@gmail.com>
2018-11-16 20:34:42 +1000

    no message

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,19 +8,19 @@ class IndexController extends BaseController{
 	public function show(){
 		echo "Inside Homepage from controller class";
 		$mail = new Mail();
-		$datas = [
+		$data = [
 			'to' => 'derrickhuang2333@gmail.com',
 			'subject'=> 'Welcome to Hxx Store',
 			'view' => 'welcome',
 			'name' => 'Hxx',
 			'body' => "Testing email template"
 			];
-		if($mail->send($data)){
-			echo "Email sent!";
-		}else{
-			echo "Email sending failed!";
-		}
-		
+		// if($mail->send($data)){
+		// 	echo "Email sent!";
+		// }else{
+		// 	echo "Email sending failed!";
+		// }
+
 	}
 }
 ?>
