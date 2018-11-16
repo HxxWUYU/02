8b45a49617eb6471da7e41200298ca228a94a47b
Derrick <derrickhuang2333@gmail.com>
2018-11-16 21:55:13 +1000

    no message

--- app/routing/route.php
+++ app/routing/route.php
@@ -4,6 +4,7 @@ $router = new AltoRouter;
 //echo $_SERVER['PHP_SELF']."<br>";
 
 $router->map('GET','/02/public/','App\Controllers\IndexController@show','home'); //@sign can be custom 
+
 $router->map('GET','/02/public/admin','App\Controllers\Admin\DashboardController@show','admin_dashboard'); //@sign can be custom 
 
 //for admin routes
