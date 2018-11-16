9744fa1d052e35eac34316b25e3a4dfd9660c0ec
Derrick <derrickhuang2333@gmail.com>
2018-11-16 14:49:41 +1000

    no message

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -5,6 +5,9 @@ if(!isset($_SESSION)) session_start();
 //load environment 
 require_once __DIR__ .'/../app/config/env.php';
 
+//instantiate database class
+new \App\Classes\Database();
+
 require_once __DIR__ .'/../app/routing/route.php';
 
 new \App\RouteDispatcher($router);
