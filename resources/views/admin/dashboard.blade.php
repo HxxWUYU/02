55a9adf844ee8dcd9d16ac91f7fd0250e68e4f3f
Derrick <derrickhuang2333@gmail.com>
2018-11-17 12:01:27 +1000

    no message

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
  <div class="dashboard">
  	<div class="row expanded">
  		<h2>Dashboard </h2>
- 		<form action="/admin" method="post">
+ 		<form action="/02/public/admin" method="post">
  			<input name = "product" value="testing">
  			<input type = "file" name ="image">
  			<input type = "submit" value="Go" name="submit">
