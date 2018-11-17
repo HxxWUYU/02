c139eb1aa709d240f9030399dd85c71800c1ad9c
Derrick <derrickhuang2333@gmail.com>
2018-11-17 10:31:28 +1000

    no message

--- app/classes/Session.php
+++ app/classes/Session.php
@@ -7,7 +7,7 @@ class Session{
 
 	public static function add($name,$value){
 		if($name!='' && !empty($name) && $value!='' &&!empty($value))｛
-			return $_SESSION[$name] = $value
+			return $_SESSION[$name] = $value;
 		｝
 
 		throw new \Exception ('Name and value required');
