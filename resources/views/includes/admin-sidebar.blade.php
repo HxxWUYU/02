1ce20b0eaae2f14714307d7a4bd2167b38a32511
Derrick <derrickhuang2333@gmail.com>
2018-11-17 17:00:26 +1000

    no message

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -1,5 +1,5 @@
 
-  <div class="off-canvas position-left" id="offCanvas" data-off-canvas>
+  <div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
 
     <!-- Close button -->
     
