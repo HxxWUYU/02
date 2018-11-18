c7b6cb3e3a7e86ad05e46355c4a96613165b3a6d
Derrick <derrickhuang2333@gmail.com>
2018-11-19 07:42:23 +1000

    no message

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -46,25 +46,29 @@
  								<td>{{$category['name']}}</td>
  								<td>{{$category['slug']}}</td>
  								<td>{{$category['added']}}</td>
- 								<td width="100" class="text-right">
- 									<a data-open="{{$category['id']}}"><i class="fa fa-edit"></i></a>
- 									<a href="#"><i class="fa fa-times"></i></a>
- 									<div class="reveal" id="item-{{$category['id']}}" data-reveal>
-									  <h1>Edit Category</h1>
-									  <form>
-						 				<div class='input-group'>
-						 					<input type="text" value="{{$category['name']}}" name="name">
-						 					<input type="hidden" name="token" value="{{\App\Classes\CSRFToken::_token()}}">
-						 					<div>
-						 						<input type="submit" class="button update-category" value="Update" id="{{$category['id']}}">
-						 					</div>
-						 				</div>
-						 			</form>
-									  <button class="close-button" data-close aria-label="Close modal" type="button">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									</div>
- 								</td>
+ 								 <td width="100" class="text-right">
+                                            <a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
+                                            <a href="#"><i class="fa fa-times"></i></a>
+                                        
+                                        <!--Edit Category Modal -->
+                                        <div class="reveal" id="item-{{$category['id']}}"
+                                             data-reveal data-close-on-click="false" data-close-on-esc="false">
+                                            <h2>Edit Category</h2>
+                                            <form>
+                                                <div class="input-group">
+                                                    <input type="text" name="name" value="{{ $category['name'] }}">
+                                                    <input type="hidden" name="token" value="{{ \App\Classes\CSRFToken::_token() }}">
+                                                    <div>
+                                                        <input type="submit" class="button update-category"
+                                                               id="{{$category['id']}}" value="Update">
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <a class="close-button" data-close aria-label="Close modal" type="button">
+                                                <span aria-hidden="true">&times;</span>
+                                            </a>
+                                        </div>
+                                    </td>
  							</tr>
  						@endforeach
  					</tbody>
