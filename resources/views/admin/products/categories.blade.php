b14956f6a9264937700124a79c4bb2da6bc81384
Derrick <derrickhuang2333@gmail.com>
2018-11-19 12:58:32 +1000

    no message

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -52,7 +52,7 @@
  											<a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
  									</span>
 	 									<!-- Delete Category-->
-	 									<span style="display:inline-block">
+	 									<span id = 'delete-span' style="display:inline-block">
 	 										<form method="POST" action="/02/public/admin/product/categories/{{$category['id']}}/delete"  class="delete-item">
 	 											<input type="hidden" name="token" value="{{\App\Classes\CSRFToken::_token()}}">
 	 											<button type="submit"><i class="fa fa-times delete"></i></button>
