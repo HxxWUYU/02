ffce8448ede2c285f73968dda76ffb3031403535
Derrick <derrickhuang2333@gmail.com>
2018-11-20 14:37:54 +1000

    no message

--- public/js/all.js
+++ public/js/all.js
@@ -92,24 +92,26 @@ this.inputs.eq(t).attr({id:i,max:this.options.end,min:this.options.start,step:th
 
 		$("#product-category").on('change',function(){
 			var category_id = $('#product-category'+" option:selected").val();
-			
-			$.ajax({
-				type:"post",
-				url:'/02/public/admin/category/'+category_id+'/selected',
-				data:{category_id:category_id},
-				success:function(response){
-					var subcategories = $.parseJSON(response);
-					if(subcategories.length>0){
-						$.each(subcategories,function(key,value){
-							$('#product-subcategory').append('<option value="'
-								+value.id+'">'+value.name+'</option>');
-						});
-					}else{
-						$('#product-subcategory').append('<option value="">No record found</option>');
-
+			$('#product-subcategory').html('Select Subcategory');
+			if(category_id.length>0){
+				$.ajax({
+					type:"post",
+					url:'/02/public/admin/category/'+category_id+'/selected',
+					data:{category_id:category_id},
+					success:function(response){
+						var subcategories = $.parseJSON(response);
+						if(subcategories.length>0){
+							$.each(subcategories,function(key,value){
+								$('#product-subcategory').append('<option value="'
+									+value.id+'">'+value.name+'</option>');
+							});
+						}else{
+							$('#product-subcategory').append('<option value="">No record found</option>');
+						}
 					}
-				}
-			});
+				});
+			}
+		
 		});
 	};
 })();
