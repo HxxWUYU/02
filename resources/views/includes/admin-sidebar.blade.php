9c1d74d6f2178529410250b16a9667affa1e23b4
Derrick <derrickhuang2333@gmail.com>
2018-11-21 08:51:56 +1000

    no message

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -12,10 +12,10 @@
     </div>
     <!-- Side bar-->
     <ul class="vertical menu">
-      <li><a href="/admin"><i class="fa fa-tachometer fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
-      <li><a href="/admin/product/create"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Product</a></li>
-      <li><a href="/admin/product/inventory"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp; Manage Product</a></li>
-      <li><a href="/admin/product/categories"><i class="fa fa-compress" aria-hidden="true"></i>&nbsp; Categories</a></li>
+      <li><a href="/02/public/admin"><i class="fa fa-tachometer fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
+      <li><a href="/02/public/admin/product/create"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Product</a></li>
+      <li><a href="/02/public/admin/product/inventory"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp; Manage Product</a></li>
+      <li><a href="/02/public/admin/product/categories"><i class="fa fa-compress" aria-hidden="true"></i>&nbsp; Categories</a></li>
       <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; View Orders</a></li>
       <li><a href="#"><i class="fa fa-money fa-fw" aria-hidden="true"></i>&nbsp; Payments</a></li>
       <li><a href="#"><i class="fa fa-sign-out fa-fw" aria-hidden="true"></i>&nbsp; Logout</a></li>
