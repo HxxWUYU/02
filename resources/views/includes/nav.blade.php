8f269979c9e4f32d3aab54340592db9b63ec6734
Derrick <derrickhuang2333@gmail.com>
2018-11-21 13:24:15 +1000

    no message

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,3 +1,6 @@
+<?php
+$categories = \App\Models\Category::width('subCategories')->get()
+?>
 <header class="navigation">
 	<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
   <button class="menu-icon" type="button" data-toggle="example-menu"></button>
@@ -9,9 +12,28 @@
  		<div class="top-bar-title show-fow-medium">
  			<a href="/" class="logo">Hxx Store</a>
  		</div>
- 		 <div class="top-bar-left">
+ <div class="top-bar-left">
     <ul class="dropdown menu vertical medium-horizontal">
       <li>Hxx Products</li>
+      @if(count($categories)>0)
+      	<li>
+      		<a href="#">Categories</a>
+      		<ul class="menu vertical sub dropdown">
+      			@foreach($categories as $category)
+      				<a href="#">{{$category->name}}</a>
+      				@if(count($category->subCategories)>0)
+      					<ul class="menu sub dropdown">
+      						@foreach($category->subCategories as $subCategory)
+      							<li>
+      								<a href="#">{{$subCategory->name}}</a>
+      							</li>
+      						@endforeach
+      					</ul>
+      				@endif
+      			@endoreach
+      		</ul>
+      	</li>
+      @endif
       
     </ul>
   </div>
