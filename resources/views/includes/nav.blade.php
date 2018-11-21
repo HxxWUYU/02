bc6b4d3893f9a7a2e6e7d8236de1baaf2ec97c89
Derrick <derrickhuang2333@gmail.com>
2018-11-21 13:18:31 +1000

    no message

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -4,27 +4,25 @@
   <div class="title-bar-title">Menu</div>
 </div>
 
-<div class="top-bar" id="example-menu">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">Site Title</li>
-      <li>
-        <a href="#">One</a>
-        <ul class="menu vertical">
-          <li><a href="#">One</a></li>
-          <li><a href="#">Two</a></li>
-          <li><a href="#">Three</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
+<div class="top-bar" id="main-menu">
+ 	<div class="menu medium-horizontal expanded medium-text-center" data-responsive-menu="drilldown medium=dropdown" data-click-open="true" data-disable-hover="true" data-close-on-click-inside="false">
+ 		<div class="top-bar-title show-fow-medium">
+ 			<a href="/" class="logo">Hxx Store</a>
+ 		</div>
+ 		 <div class="top-bar-left">
+    <ul class="dropdown menu vertical medium-horizontal">
+      <li>Hxx Products</li>
+      
     </ul>
   </div>
   <div class="top-bar-right">
-    <ul class="menu">
-      <li><input type="search" placeholder="Search"></li>
-      <li><button type="button" class="button">Search</button></li>
+    <ul class="dropdown menu vertical medium-horizontal">
+    	<li>Username</li>
+    	<li><a href="#">Sign In</a></li>
+    	<li><a href="#">Register</a></li>
+    	<li><a href="#">Cart</a></li>  
     </ul>
   </div>
+ 	</div>
 </div>
 </header>
