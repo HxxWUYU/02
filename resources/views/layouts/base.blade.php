afb14e43f4cd5544c9b979f67dc83fb47d23d337
Derrick <derrickhuang2333@gmail.com>
2018-11-21 13:11:04 +1000

    no message

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width， initial-scale=1.0">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Hxx Store - @yield('title')</title>
 	<link rel="stylesheet" href="/02/public/css/all.css">
 	<script src="https://use.fontawesome.com/82337c0b5c.js"></script>
