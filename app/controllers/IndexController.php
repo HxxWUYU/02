5b63ef941225f8863c504936800410dd8eceab5b
Derrick <derrickhuang2333@gmail.com>
2018-11-23 18:46:12 +1000

    no message

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -26,10 +26,10 @@ class IndexController extends BaseController{
 	}
 
 	public function featuredProducts(){
-		// $products = Product::where('featured',1)->inRandomOrder()->limt(4)->get();
+		
 
-			
-		$products = Product::where('featured', 1)->inRandomOrder()->limit(4)->get();
+
+		$products = Product::where('featured',1)->inRandomOrder()->limit(4)->get();
 
 		echo json_encode(['featured'=>$products]);
 
