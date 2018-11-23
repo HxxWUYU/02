000ad66b4a340ac9c3588fbce68085df4af61d5b
Derrick <derrickhuang2333@gmail.com>
2018-11-23 11:33:22 +1000

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,9 @@
 	<div class="home">
 		<section class="hero">
 			<div class="hero-slider">
-				<div img src="02/public/images/sliders/01.jpg alt='Hxx Store' ">
-				<div img src="02/public/images/sliders/02.jpg alt='Hxx Store' ">
-				<div img src="02/public/images/sliders/03.jpg alt='Hxx Store' ">
-					
-				</div>
+				<div img src="02/public/images/sliders/01.jpg alt='Hxx Store' "></div>
+				<div img src="02/public/images/sliders/02.jpg alt='Hxx Store' "></div>
+				<div img src="02/public/images/sliders/03.jpg alt='Hxx Store' "></div>
 			</div>
 		</section>
 	</div>
