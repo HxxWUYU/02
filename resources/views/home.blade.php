8ef1cd3b975c109a074e138e81084b263d2d73bd
Derrick <derrickhuang2333@gmail.com>
2018-11-24 13:56:49 +1000

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,10 +21,10 @@
 		
 		<section class="display-products" id="root">
 			<!-- <div class="row medium-up-4 feature-slider"> -->
-			<div class="row medium-up-4">
+			<div class="row medium-up-4  feature-slider">
 				<h2>Featured Products</h2>
 				
-				<div class="small-12 column feature-slider " v-for="feature in featured">
+				<div class="small-12 column " v-for="feature in featured">
 				<!-- <div class="column" v-for="feature in featured"> -->
 					<a :href="'/product/'+feature.id">
 						<div class="card" data-equalizer-watch>
