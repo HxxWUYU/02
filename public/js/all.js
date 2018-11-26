238560004d1c509eac0c02939a64dd8c3b69d6ed
Derrick <derrickhuang2333@gmail.com>
2018-11-26 15:33:24 +1000

    no message

--- public/js/all.js
+++ public/js/all.js
@@ -450,38 +450,6 @@ e.exports=function(e){return null!=e&&(n(e)||r(e)||!!e._isBuffer)}},function(e,t
 		
 	}
 
-	new Vue({
-	    components: { Slick },
-	    data() {
-	            return {
-	                    slickOptions: {
-	                        //options can be used from the plugin documentation
-	                        slidesToShow: 4,
-	                        infinite: true,
-	                        accessibility: true,
-	                        adaptiveHeight: false,
-	                        arrows: true,
-	                        dots: true,
-	                        draggable: true,
-	                        edgeFriction: 0.30,
-	                        swipe: true
-	                    }
-	            }
-	    },
-	    // All slick methods can be used too, example here
-	    methods: {
-	            next() {
-	                    this.$refs.slick.next()
-	            },
-	            prev() {
-	                    this.$refs.slick.prev()
-	            },
-	            reInit() {
-	                    // Helpful if you have to deal with v-for to update dynamic lists
-	                    this.$refs.slick.reSlick()
-	            }
-	    }
-	});
 })();
 (function(){
 	'use strict';
