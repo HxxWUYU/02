d69e494067ec3bf121b62d17ed2d7fb95b8d0d16
Derrick <derrickhuang2333@gmail.com>
2018-11-26 18:31:15 +1000

    no message

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -109,8 +109,8 @@
 								
 								<button type="submit" class="button alert float-left">EmptyChart &nbsp<i class="fa fa-exclamation-triangle" aria-hidden="true"></i></button>
 							
-							<a href="/02/public/" class="button secondary float-right" style="margin-right: 0.5rem">Continue Shopping &nbsp<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
-							<button type="submit" class="button success float-right">Checkout &nbsp<i class="fa fa-credit-card-alt" aria-hidden="true"></i></button>
+							<a href="/02/public/" class="button secondary float-right" >Continue Shopping &nbsp<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+							<button type="submit" class="button success float-right" style="margin-right: 0.5rem;">Checkout &nbsp<i class="fa fa-credit-card-alt" aria-hidden="true"></i></button>
 							</div>
 						
 					</div>
