f9ef4b85427ead10a9218393c2d27500697de9c2
Derrick <derrickhuang2333@gmail.com>
2018-11-26 14:07:47 +1000

    no message

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -17,6 +17,54 @@
 					<h2 v-if='fail' v-text="message"></h2>
 					<div v-else>
 						<h2>Your Cart</h2>
+						<table class="hover unstriped">
+							<thead class="text-left">
+								<tr>
+									<th>#</th>
+								<th>Product Name</th>
+								<th>Price</th>
+								<th>Qty</th>
+								<th>Total</th>
+								<th>Action</th>	
+								</tr>
+								<tbody>
+									<tr v-for="item in items">
+										<td class="medium-text-center">
+											<a :href="'/02/public/product'+item.id">
+												<img src="'/02/public/'+item.image" height="60px" width="60px" alt="item.name">
+											</a>
+										</td>
+										<td>
+											<h5>
+												<a :href="'/02/public/product'+item.id">@{{item.name}}</a>
+												Status:
+												<span v-if="item.stock>1" style="color:#00AA00;">
+													In Stock
+												</span>
+												<span v-else style="color:#ff0000;">
+													Out of Stock
+												</span>
+											</h5>
+										</td>
+										<td>
+											@{{item.price}}
+										</td>
+										<td>
+											@{{item.quantity}}
+										</td>
+										<td>
+											@{{item.total}}
+										</td>
+										<td>
+											<button>
+												<i class="fa fa-times" aria-hidden="true"></i>
+											</button>
+										</td>
+									</tr>
+								</tbody>
+								
+							</thead>
+						</table>
 					</div>
 				</div>
 			</div>
