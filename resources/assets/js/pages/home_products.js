75a2279da9b622a042aa8157b85dc5815c166944
Derrick <derrickhuang2333@gmail.com>
2018-11-26 23:50:20 +1000

    no message

--- resources/assets/js/pages/home_products.js
+++ resources/assets/js/pages/home_products.js
@@ -33,7 +33,7 @@
 				},
 				addToCart:function(id){
 					 HXXSTORE.module.addItemToCart(id,function(message){
-					 	$('.notify').slideDown(400).delay(4000).slideUp(300).html(message);
+					 	$('.notify').css('display','none').slideDown(400).delay(4000).slideUp(300).html(message);
 					});
 				},
 
