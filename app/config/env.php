d4d469859a706c307c02822adbe29a82ffae1b4f
Derrick <derrickhuang2333@gmail.com>
2018-11-27 12:31:19 +1000

    no message

--- app/config/env.php
+++ app/config/env.php
@@ -2,9 +2,11 @@
 define("BASE_PATH",realpath(__DIR__.'/../../'));
 
 require_once __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__ . '/_stripe.php';
+
 
 $dotEnv = new \Dotenv\Dotenv(BASE_PATH);
 
 $dotEnv->load();
+
+require_once __DIR__ . '/_stripe.php';
 ?>
